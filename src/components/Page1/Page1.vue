17f88158e66b6b6edef688eb337e72852c47317f
Tuusoo <suhao1998@yeah.net>
2021-09-24 00:06:47 +0800

    引入图片等等~

---
+++ src/components/Page1/Page1.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="bg">
+    <div class="bar">
+      <div class="logo">
+        SUBJECT
+        <span>EDUCATION</span>
+      </div>
+      <div class="buttons">
+        <div class="button">Home</div>
+        <div class="button">About Us</div>
+        <div class="button">Dropdown</div>
+        <div class="button">Contact Us</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.bg {
+  background: url(../../imgs/bg1.jpg) center no-repeat;
+  background-size: cover;
+  
+  .bar {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .logo {
+      color: #FFFFFF;
+      font-size: 38px;
+      font-family: 'Montserrat', sans-serif;
+      font-weight: 700;
+      letter-spacing: 2px;
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      margin: 20px 0 0 80px;
+      user-select: none;
+      cursor: pointer;
+      span {
+        font-size: 8px;
+        font-family: 'Roboto', sans-serif;
+        letter-spacing: 6px;
+        text-align: center;
+      }
+    }
+    .buttons {
+      display: flex;
+      align-items: center;
+      color: #FFFFFF;
+    }
+  }
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <Page1 />
+</template>
+
+<script>
+import Page1 from '../components/Page1/Page1.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Page1,
+  }
+}
+</script>
